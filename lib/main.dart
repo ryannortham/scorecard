b6d3c03c318677e2eaaf20c68f8a88a31cbac19d
Ryan Northam <ryan.northam@gmail.com>
2023-05-04 14:28:32 +1000

    separate main from page file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'landing_page.dart';
 
 void main() {
   runApp(const GoalKeeper());
@@ -15,75 +16,6 @@ class GoalKeeper extends StatelessWidget {
         primarySwatch: Colors.green,
       ),
       home: const LandingPage(title: 'GoalKeeper'),
-    );
-  }
-}
-
-class LandingPage extends StatefulWidget {
-  const LandingPage({super.key, required this.title});
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<LandingPage> createState() => _LandingPageState();
-}
-
-class _LandingPageState extends State<LandingPage> {
-  @override
-  Widget build(BuildContext context) {
-    final primaryButtonStyle = ElevatedButton.styleFrom(
-      backgroundColor: Theme.of(context).primaryColor,
-      elevation: 0,
-    );
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            const Spacer(flex: 8),
-            Text(
-              'Welcome to GoalKeeper',
-              style: Theme.of(context).textTheme.headlineSmall,
-            ),
-            const Spacer(flex: 2),
-            Text(
-              'What would you like to do?',
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            const Spacer(flex: 1),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.75,
-              child: ElevatedButton(
-                style: primaryButtonStyle,
-                onPressed: () {},
-                child: const Text("Score Keeping"),
-              ),
-            ),
-            const Spacer(flex: 1),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.75,
-              child: ElevatedButton(
-                style: primaryButtonStyle,
-                onPressed: () {},
-                child: const Text("View Game Results"),
-              ),
-            ),
-            const Spacer(flex: 8),
-          ],
-        ),
-      ),
     );
   }
 }
